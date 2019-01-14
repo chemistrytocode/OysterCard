1df4b0caf47864cd2e2de1e9a5d0f309bd1d20e2
Ross <ross.hunter@tesco.com>
2019-01-14 14:10:29 +0000

    Top up method for oystercard added

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,8 +2,16 @@ require 'oystercard'
 
 describe Oystercard do
 
-  it 'Default card balance equals zero' do
-    expect(subject.balance).to eq(0)
+  it {is_expected.to respond_to(:top_up).with(1).arguments}
+
+  it 'Should top up oystercard with money' do
+    expect{subject.top_up 400 }.to change {subject.balance}.by 400
   end
+
+  it 'Default card balance equals zero' do
+    expect(subject.balance).to eq 0
+
+  end
+
 
 end
