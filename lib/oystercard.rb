3a4c57c767732dc914b465dbb5a1461936b8fbda
your name <saule.guzyte@gmail.com>
2019-01-14 14:54:14 +0000

    Enforced card maxium balance

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -8,7 +8,13 @@ attr_accessor :balance
   end
 
   def top_up(money)
-    @balance += money
+    total = @balance + money
+  fail "You can only top up with a maximum of £90" if money > MAXIMUM_BALANCE
+  fail "Topping up this amount will exceed your maximum balance of £90" if total > MAXIMUM_BALANCE
+  @balance += money
   end
 
 end
+private
+
+MAXIMUM_BALANCE = 90
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -5,13 +5,22 @@ describe Oystercard do
   it {is_expected.to respond_to(:top_up).with(1).arguments}
 
   it 'Should top up oystercard with money' do
-    expect{subject.top_up 400 }.to change {subject.balance}.by 400
+    expect{subject.top_up 90 }.to change {subject.balance}.by 90
   end
 
   it 'Default card balance equals zero' do
     expect(subject.balance).to eq 0
-
   end
 
+  it 'Will not allow you to top up more than £90 at once' do
+    message = "You can only top up with a maximum of £90"
+    expect{subject.top_up(100)}.to raise_error message
+  end
+
+  it 'Will not allow you to exceed maximum balance of £90 on card' do
+    subject.top_up(50)
+    message = "Topping up this amount will exceed your maximum balance of £90"
+    expect{subject.top_up(50)}.to raise_error message
+  end
 
 end
