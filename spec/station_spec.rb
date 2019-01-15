4d7d6448e5f439c77ff07c2b3220580901577eea
your name <saule.guzyte@gmail.com>
2019-01-15 15:34:37 +0000

    Station class with name and zone variables created

---
+++ spec/station_spec.rb
@@ -0,0 +1,15 @@
+require 'station'
+describe Station do
+
+  subject {described_class.new("Old Street", 1)}
+
+  describe '#new' do
+    it 'it is given a name' do
+      expect(subject.name).to eq("Old Street")
+    end
+    it 'it is given a zone' do
+      expect(subject.zone).to eq(1)
+    end
+  end
+
+end
